cdd43c6f68ab11bfcb0dfca85afa533671457d11
Rheitheillaethor <minegocu3@gmail.com>
2021-11-08 10:53:08 +0100

    Add project files.

---
+++ EncryptionDecryption/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace EncryptionDecryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "ABCDEFGHI";
+            int i = 0;
+            int[] array1 = new int[str.Length];
+            foreach (var c in str)
+            {
+                array1[i++] = c;
+            }
+            char character = (char)array1[2];
+            string test = character.ToString();
+            Console.WriteLine(test);
+
+
+        }
+    }
+}
