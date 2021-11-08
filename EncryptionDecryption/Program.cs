bf94618e0a2932f6b7c00cf0bee0ad6ffb59c671
Rheitheillaethor <minegocu3@gmail.com>
2021-11-08 17:10:33 +0100

    TryCatch is catching wrong numbers, program asks for wish to try again. Last thing what will be nice to do is: crypted words in string, not in single chars

--- EncryptionDecryption/Program.cs
+++ EncryptionDecryption/Program.cs
@@ -9,48 +9,76 @@ namespace EncryptionDecryption
         {
             EnDeCrypt();
         }
-    static void EnDeCrypt()
-    {
-        Console.WriteLine("Write text which you want to encrypt");
-        string givenText = Console.ReadLine();
-        Console.WriteLine("Write number bettwen (-6, 6)");
-        int cryptionNumber = Convert.ToInt32(Console.ReadLine());
-        int[] CharArray = new int[givenText.Length];
-        int i = 0;
-        foreach (var c in givenText)
+        static void EnDeCrypt()
         {
-            CharArray[i++] = c;
-        }
-        for (int j = 0; j < givenText.Length; j++)
-        {
-            if ((Enumerable.Range(65, 90).Contains(CharArray[j])) || (Enumerable.Range(97, 122).Contains(CharArray[j])))
-            {
-                CharArray[j] += cryptionNumber;
-                if (CharArray[j] < 65)
-                {
-                    CharArray[j] += 26;
-                }
-                else if (CharArray[j] > 122)
-                {
-                    CharArray[j] -= 26;
-                }
-                else if (CharArray[j] > 90 && CharArray[j] < 97)
-                {
-                    CharArray[j] = cryptionNumber > 0 ? CharArray[j] -= 26 : CharArray[j] += 26;
-                }
-                char character = (char)CharArray[j];
-                string test = character.ToString();
-                Console.WriteLine(test);
-            }
-            else
-            {
-                char character = (char)CharArray[j];
-                string test = character.ToString();
-                Console.WriteLine(test);
-            }
 
+            Console.WriteLine("Write text which you want to encrypt");
+            string givenText = Console.ReadLine();
+            Console.WriteLine("Write number bettwen (-6, 6)");
+            string givenNumber = Console.ReadLine();
+            try
+            { 
+                int cryptionNumber = int.Parse(givenNumber);
+                if (cryptionNumber > 6 || cryptionNumber < -6)
+                {
+                    Console.WriteLine("Wrong number");
+                    Console.WriteLine("Try again!");
+                    EnDeCrypt();
+                }
+                int[] CharArray = new int[givenText.Length];
+                int i = 0;
+                foreach (var c in givenText)
+                {
+                    CharArray[i++] = c;
+                }
+                for (int j = 0; j < givenText.Length; j++)
+                {
+                    if ((Enumerable.Range(65, 90).Contains(CharArray[j])) || (Enumerable.Range(97, 122).Contains(CharArray[j])))
+                    {
+                        CharArray[j] += cryptionNumber;
+                        if (CharArray[j] < 65)
+                        {
+                            CharArray[j] += 26;
+                        }
+                        else if (CharArray[j] > 122)
+                        {
+                            CharArray[j] -= 26;
+                        }
+                        else if (CharArray[j] > 90 && CharArray[j] < 97)
+                        {
+                            CharArray[j] = cryptionNumber > 0 ? CharArray[j] -= 26 : CharArray[j] += 26;
+                        }
+                        char character = (char)CharArray[j];
+                        string test = character.ToString();
+                        Console.WriteLine(test);
+                    }
+                    else
+                    {
+                        char character = (char)CharArray[j];
+                        string test = character.ToString();
+                        Console.WriteLine(test);
+                    }
+
+                }
+                Console.WriteLine("Do you want to try again?");
+                Console.WriteLine("'yes' or anything for no");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    EnDeCrypt();
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for using my program");
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not an integer", givenNumber);
+                Console.WriteLine("Try again!");
+                EnDeCrypt();
+            }
+            }
         }
-        EnDeCrypt();
-    }
-}
 }
