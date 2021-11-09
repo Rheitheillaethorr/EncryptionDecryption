72ceb4b8b7cbdfd8a674a6e763ae530f3df2db70
Rheitheillaethor <minegocu3@gmail.com>
2021-11-09 14:34:59 +0100

    Now console returns string, I could not overwrite variables and have to create new array. It should have been committed yesterday

--- EncryptionDecryption/Program.cs
+++ EncryptionDecryption/Program.cs
@@ -11,13 +11,15 @@ namespace EncryptionDecryption
         }
         static void EnDeCrypt()
         {
-
+            
             Console.WriteLine("Write text which you want to encrypt");
             string givenText = Console.ReadLine();
-            Console.WriteLine("Write number bettwen (-6, 6)");
+            Console.WriteLine("Write number between (-6, 6)");
+            Console.WriteLine("If you use for example encryption with number 2, you can decrypt it with -2");
             string givenNumber = Console.ReadLine();
+            string[] AnswerArray = new string[givenText.Length];
             try
-            { 
+            {
                 int cryptionNumber = int.Parse(givenNumber);
                 if (cryptionNumber > 6 || cryptionNumber < -6)
                 {
@@ -33,6 +35,7 @@ namespace EncryptionDecryption
                 }
                 for (int j = 0; j < givenText.Length; j++)
                 {
+
                     if ((Enumerable.Range(65, 90).Contains(CharArray[j])) || (Enumerable.Range(97, 122).Contains(CharArray[j])))
                     {
                         CharArray[j] += cryptionNumber;
@@ -48,18 +51,15 @@ namespace EncryptionDecryption
                         {
                             CharArray[j] = cryptionNumber > 0 ? CharArray[j] -= 26 : CharArray[j] += 26;
                         }
-                        char character = (char)CharArray[j];
-                        string test = character.ToString();
-                        Console.WriteLine(test);
                     }
-                    else
-                    {
-                        char character = (char)CharArray[j];
-                        string test = character.ToString();
-                        Console.WriteLine(test);
-                    }
-
                 }
+                for (int l = 0;l < givenText.Length; l++)
+                {
+                    char character = (char)CharArray[l];
+                    string charToString = character.ToString();
+                    AnswerArray[l] = charToString;
+                }
+                Console.WriteLine(String.Join("", AnswerArray));
                 Console.WriteLine("Do you want to try again?");
                 Console.WriteLine("'yes' or anything for no");
                 string answer = Console.ReadLine();
@@ -79,6 +79,6 @@ namespace EncryptionDecryption
                 Console.WriteLine("Try again!");
                 EnDeCrypt();
             }
-            }
         }
+    }
 }
